733ebff809cc0d5adb3bbfb8c9fa1aa062d5c159
Yassien <11986226+YassienW@users.noreply.github.com>
2020-10-12 17:27:30 +0200

    Remove mirage

--- src/views/index.js
+++ src/views/index.js
@@ -5,9 +5,5 @@ import App from "./App";
 import "rsuite/dist/styles/rsuite-default.css"
 import "../../res/styles.less";
 
-/*if (process.env.NODE_ENV !== "production"){
-    require("../mock/Mirage").startMirage();
-}*/
-
 const HotApp = hot(module)(App);
 ReactDOM.render(<HotApp/>, document.getElementById("app"));
